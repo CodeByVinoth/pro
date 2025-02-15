9256b870f42027fd47c66391ea20b23db10ecba3
CodeByVinoth <161485638+CodeByVinoth@users.noreply.github.com>
2025-02-15 20:21:14 +0530

    Initial commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="bg-black text-white text-center w-full mt-auto relative z-5">
       <p className="text-lg font-medium">
-        &copy; {new Date().getFullYear()} Wattstrons. All rights reserved. Designed by    
+        &copy; {new Date().getFullYear()} Wattstrons. All rights reserved.    
       </p>
     </footer>
   );
